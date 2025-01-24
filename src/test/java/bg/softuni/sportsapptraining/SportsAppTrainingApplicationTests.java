e999f9d578151d5b921534c32ec90b92f3be8029
NicolaHristov <124876291+NicolaHristov@users.noreply.github.com>
2025-01-24 11:55:05 +0200

    Initial commit

---
+++ src/test/java/bg/softuni/sportsapptraining/SportsAppTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.sportsapptraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsAppTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
