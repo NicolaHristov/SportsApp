e857abe915cfc4964b8d974de4ffd2da55565189
NicolaHristov <124876291+NicolaHristov@users.noreply.github.com>
2025-02-01 13:45:16 +0200

    add new entity in project

---
+++ src/main/java/bg/softuni/sportsapptraining/model/Discipline.java
@@ -0,0 +1,68 @@
+package bg.softuni.sportsapptraining.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name ="disciplines")
+public class Discipline {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "world_record_holder")
+    private String worldRecordHolder;
+
+    @Column(name = "world_record_time")
+    private String worldRecordTime;
+
+    @ManyToOne
+    private Sport sport;
+
+    public Discipline() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWorldRecordHolder() {
+        return worldRecordHolder;
+    }
+
+    public void setWorldRecordHolder(String worldRecordHolder) {
+        this.worldRecordHolder = worldRecordHolder;
+    }
+
+    public String getWorldRecordTime() {
+        return worldRecordTime;
+    }
+
+    public void setWorldRecordTime(String worldRecordTime) {
+        this.worldRecordTime = worldRecordTime;
+    }
+
+    public Sport getSport() {
+        return sport;
+    }
+
+    public void setSport(Sport sport) {
+        this.sport = sport;
+    }
+}
---
+++ src/main/java/bg/softuni/sportsapptraining/model/Sport.java
@@ -0,0 +1,47 @@
+package bg.softuni.sportsapptraining.model;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "sports")
+public class Sport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false,unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "sport")
+    private Set<Discipline> disciplines;
+
+    public Sport() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Discipline> getDisciplines() {
+        return disciplines;
+    }
+
+    public void setDisciplines(Set<Discipline> disciplines) {
+        this.disciplines = disciplines;
+    }
+}
