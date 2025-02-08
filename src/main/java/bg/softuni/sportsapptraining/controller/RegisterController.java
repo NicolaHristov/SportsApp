0fd9f800691b5c8db1a8d24e1bd52b62b233f30f
NicolaHristov <124876291+NicolaHristov@users.noreply.github.com>
2025-02-08 10:37:06 +0200

    add ModelAttribute

--- src/main/java/bg/softuni/sportsapptraining/controller/RegisterController.java
+++ src/main/java/bg/softuni/sportsapptraining/controller/RegisterController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -15,8 +16,14 @@ public class RegisterController {
 
     private final UserService userService;
 
+
     public RegisterController(UserService userService) {
         this.userService = userService;
+    }
+
+    @ModelAttribute("registerData")
+    public RegisterDto registerDto(){
+        return new RegisterDto();
     }
 
     @GetMapping("/register")
