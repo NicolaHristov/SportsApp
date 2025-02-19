4d2c2c9ff4d5d32caf7e8d38b119940e9e4176f3
NicolaHristov <124876291+NicolaHristov@users.noreply.github.com>
2025-02-19 10:17:07 +0200

    add userSession in LoginController

--- src/main/java/bg/softuni/sportsapptraining/controller/LoginController.java
+++ src/main/java/bg/softuni/sportsapptraining/controller/LoginController.java
@@ -1,11 +1,13 @@
 package bg.softuni.sportsapptraining.controller;
 
+import bg.softuni.sportsapptraining.config.UserSession;
 import bg.softuni.sportsapptraining.model.dto.UserLoginDto;
 import bg.softuni.sportsapptraining.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -13,19 +15,32 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class LoginController {
 
     private final UserService userService;
+    private final UserSession userSession;
 
-    public LoginController(UserService userService) {
+    @ModelAttribute("loginData")
+    public UserLoginDto loginDto(){
+        return new UserLoginDto();
+    }
+
+    public LoginController(UserService userService, UserSession userSession) {
         this.userService = userService;
+        this.userSession = userSession;
     }
 
     @GetMapping("/login")
     public String login(){
-
+       if(userSession.isUserLoggedIn()){
+           return "redirect:/home";
+       }
         return "login";
     }
 
     @PostMapping("/login")
     public String doLogin(@Valid UserLoginDto data, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+
+        if(userSession.isUserLoggedIn()){
+            return "redirect:/home";
+        }
 
         if(bindingResult.hasErrors()){
              redirectAttributes.addFlashAttribute("loginData",data);
