c0b4611c6651557d898a8c7d2626d9331f8c9230
NicolaHristov <124876291+NicolaHristov@users.noreply.github.com>
2025-03-11 16:06:24 +0200

    add CommentRepository and CommentService

---
+++ src/main/java/bg/softuni/sportsapptraining/service/CommentService.java
@@ -0,0 +1,29 @@
+package bg.softuni.sportsapptraining.service;
+
+import bg.softuni.sportsapptraining.model.Comment;
+import bg.softuni.sportsapptraining.model.Discipline;
+import bg.softuni.sportsapptraining.model.User;
+import bg.softuni.sportsapptraining.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+
+
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public List<Comment> getCommentsForDiscipline(Discipline discipline) {
+        return commentRepository.findByDiscipline(discipline);
+    }
+
+    public void addComment(String text, User user, Discipline discipline) {
+        Comment comment = new Comment(text, user, discipline);
+        commentRepository.save(comment);
+    }
+}
