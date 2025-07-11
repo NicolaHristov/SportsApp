20f670aa6e5776830281ac3271b7239e0bdf0565
NikolaHristov <swimmingman23@abv.bg>
2025-07-11 14:53:11 +0300

    refactor(HomeController): remove unused constructor and Principal param

--- src/main/java/bg/softuni/sportsapptraining/controller/HomeController.java
+++ src/main/java/bg/softuni/sportsapptraining/controller/HomeController.java
@@ -3,18 +3,11 @@ package bg.softuni.sportsapptraining.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.security.Principal;
-
 @Controller
 public class HomeController {
 
-
-    public HomeController() {
-
-    }
-
     @GetMapping("/")
-    public String notLogged(Principal principal) {
+    public String notLogged() {
         return "index";
     }
 
