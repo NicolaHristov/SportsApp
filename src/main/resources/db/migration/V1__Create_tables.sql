789bb989fd0b6d7c1bd57d8e57e1bb6d8ad8df36
NikolaHristov <swimmingman23@abv.bg>
2025-07-21 16:21:06 +0300

    feat: integrate PlanetScale and enable Flyway migrations for production

--- src/main/resources/db/migration/V1__Create_tables.sql
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE users (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(50) NOT NULL UNIQUE,
@@ -7,29 +6,23 @@ CREATE TABLE users (
   role     VARCHAR(20)  NOT NULL
 );
 
-
 CREATE TABLE sports (
   id   BIGINT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100)     NOT NULL UNIQUE
+  name VARCHAR(100) NOT NULL UNIQUE
 );
-
 
 CREATE TABLE disciplines (
-  id                   BIGINT AUTO_INCREMENT PRIMARY KEY,
-  name                 VARCHAR(100) NOT NULL,
-  world_record_holder  VARCHAR(255),
-  world_record_time    VARCHAR(255),
-  sport_id             BIGINT       NOT NULL,
-  FOREIGN KEY (sport_id) REFERENCES sports(id)
+  id                  BIGINT AUTO_INCREMENT PRIMARY KEY,
+  name                VARCHAR(100) NOT NULL,
+  world_record_holder VARCHAR(255),
+  world_record_time   VARCHAR(255),
+  sport_id            BIGINT NOT NULL
 );
-
 
 CREATE TABLE comments (
   id            BIGINT AUTO_INCREMENT PRIMARY KEY,
   content       VARCHAR(500) NOT NULL,
   date_time     DATETIME      NOT NULL,
   user_id       BIGINT        NOT NULL,
-  discipline_id BIGINT        NOT NULL,
-  FOREIGN KEY (user_id)       REFERENCES users(id),
-FOREIGN KEY (discipline_id) REFERENCES disciplines(id)
+  discipline_id BIGINT        NOT NULL
 );
