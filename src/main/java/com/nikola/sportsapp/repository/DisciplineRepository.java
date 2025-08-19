18f9e6cc116b5febc48476320e8d3b87be95507e
NikolaHristov <swimmingman23@abv.bg>
2025-08-19 14:31:13 +0300

    refactor: remove unused and redundant methods from DisciplineRepository

--- src/main/java/com/nikola/sportsapp/repository/DisciplineRepository.java
+++ src/main/java/com/nikola/sportsapp/repository/DisciplineRepository.java
@@ -5,13 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface DisciplineRepository extends JpaRepository<Discipline,Long> {
+
     Discipline findByName(String name);
 
-    Optional<Discipline> findById(Long id);
-    boolean existsByName(String name);
     List<Discipline> findAllBySportName(String name);
+
 }
