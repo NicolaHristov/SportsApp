fcbfb395d916cdecf433c7cbb0418213e6bb6056
NikolaHristov <swimmingman23@abv.bg>
2025-08-31 14:46:02 +0300

    eat(user): introduce UserDto for exposing safe user data

--- src/main/java/com/nikola/sportsapp/model/dto/UserDto.java
+++ src/main/java/com/nikola/sportsapp/model/dto/UserDto.java
@@ -1,4 +1,17 @@
 package com.nikola.sportsapp.model.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserDto {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String role;
+
 }
