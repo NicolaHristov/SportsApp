c4cf751f6a038a561436f9024e6ad9f3cbe7961e
NikolaHristov <swimmingman23@abv.bg>
2025-09-05 14:10:50 +0300

    chore: complete migration from PlanetScale to Railway PostgreSQL

--- src/main/java/com/nikola/sportsapp/SportsAppTrainingApplication.java
+++ src/main/java/com/nikola/sportsapp/SportsAppTrainingApplication.java
@@ -2,7 +2,6 @@ package com.nikola.sportsapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class SportsAppTrainingApplication {
 
