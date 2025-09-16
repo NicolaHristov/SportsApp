6ae0aef7a4bf2dba808b149793059861c8c25193
NikolaHristov <swimmingman23@abv.bg>
2025-09-16 15:07:14 +0300

    feat: add query for fetching comments with users by disciplineId

--- src/main/java/com/nikola/sportsapp/repository/CommentRepository.java
+++ src/main/java/com/nikola/sportsapp/repository/CommentRepository.java
@@ -3,13 +3,17 @@ package com.nikola.sportsapp.repository;
 import com.nikola.sportsapp.model.Comment;
 import com.nikola.sportsapp.model.Discipline;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findByDiscipline(Discipline discipline);
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.discipline.id = :disciplineId")
+    List<Comment> findAllByDisciplineIdWithUser(@Param("disciplineId") Long disciplineId);
 
 }
--- src/main/java/com/nikola/sportsapp/service/CommentService.java
+++ src/main/java/com/nikola/sportsapp/service/CommentService.java
@@ -3,30 +3,41 @@ package com.nikola.sportsapp.service;
 import com.nikola.sportsapp.model.Comment;
 import com.nikola.sportsapp.model.Discipline;
 import com.nikola.sportsapp.model.User;
+import com.nikola.sportsapp.model.dto.CommentDto;
 import com.nikola.sportsapp.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final ModelMapper modelMapper;
 
-    public CommentService(CommentRepository commentRepository) {
+    public CommentService(CommentRepository commentRepository, ModelMapper modelMapper) {
         this.commentRepository = commentRepository;
+        this.modelMapper = modelMapper;
     }
 
-    public void addComment(String text, User user, Discipline discipline) {
-        Comment comment = new Comment(text, user, discipline);
+    public void addComment(String content, User user, Discipline discipline) {
+        Comment comment = new Comment(content, user, discipline);
         commentRepository.save(comment);
     }
 
-    public Comment save(Comment comment) {
-        return this.commentRepository.save(comment);
+
+    public List<CommentDto> findAllByDiscipline(Long disciplineId) {
+        return commentRepository.findAllByDisciplineIdWithUser(disciplineId)
+                .stream()
+                .map(comment -> modelMapper.map(comment, CommentDto.class))
+                .collect(Collectors.toList());
     }
 
-    public List<Comment> findByDiscipline(Discipline discipline) {
-        return commentRepository.findByDiscipline(discipline);
+
+    public Comment save(Comment comment) {
+        return commentRepository.save(comment);
     }
 }
