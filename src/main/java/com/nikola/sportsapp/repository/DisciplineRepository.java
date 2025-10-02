04b0a2788298ace5a69f008ac03a7168497a05e2
NikolaHristov <swimmingman23@abv.bg>
2025-10-02 14:13:21 +0300

    feat: add custom queries with JOIN FETCH in DisciplineRepository

--- src/main/java/com/nikola/sportsapp/repository/DisciplineRepository.java
+++ src/main/java/com/nikola/sportsapp/repository/DisciplineRepository.java
@@ -2,9 +2,12 @@ package com.nikola.sportsapp.repository;
 
 import com.nikola.sportsapp.model.Discipline;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DisciplineRepository extends JpaRepository<Discipline,Long> {
@@ -12,5 +15,14 @@ public interface DisciplineRepository extends JpaRepository<Discipline,Long> {
     Discipline findByName(String name);
 
     List<Discipline> findAllBySportName(String name);
+
+    @Query("SELECT d FROM Discipline d JOIN FETCH d.sport WHERE d.sport.name = :sportName")
+    List<Discipline> findAllBySportNameWithSport(@Param("sportName") String sportName);
+
+    @Query("SELECT d FROM Discipline d JOIN FETCH d.sport WHERE d.name = :name")
+    Discipline findByNameWithSport(@Param("name") String name);
+
+    @Query("SELECT d FROM Discipline d JOIN FETCH d.sport WHERE d.id = :id")
+    Optional<Discipline> findByIdWithSport(@Param("id") Long id);
 
 }
